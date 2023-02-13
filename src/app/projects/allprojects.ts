88c5e90e93b85e28f10704afd405cd6a395ef8f1
Demian Ferreyra <demiancra@gmail.com>
2023-02-13 12:47:11 -0300

    new project and resume added

--- src/app/projects/allprojects.ts
+++ src/app/projects/allprojects.ts
@@ -10,24 +10,30 @@ const project2:Object = {
     desc: "Proyecto grupal soyHenry. Para este proyecto, realizamos una red social completa, con posibilidad de entrar como invitado, con una cuenta creada, o como admin, teniendo privilegios distintos segun como entrases. La misma cuenta con una pasarela de pago para poder comprar y vender obras (En el deploy solo utilizamos la version de sandbox), asi como en el apartado de admin, una seccion para ver las transacciones realizadas",
     img: "assets/images/PG1.png"
 }
-const project3:Object = {
-    name : "Instant",
-    link:"https://github.com/DemianFerreyra/INSTANT",
-    desc: "Proyecto personal imitando instagram. Nunca fue deployado, ya que en ese tiempo no tenia conocimientos de base de datos y para almacenar imagenes se hizo uso de conversores, guardando las imagenes en un archivo .txt y como base64",
-    img: "assets/images/Instant1.png"
-}
+// const project3:Object = {
+//     name : "Instant",
+//     link:"https://github.com/DemianFerreyra/INSTANT",
+//     desc: "Proyecto personal imitando instagram. Nunca fue deployado, ya que en ese tiempo no tenia conocimientos de base de datos y para almacenar imagenes se hizo uso de conversores, guardando las imagenes en un archivo .txt y como base64",
+//     img: "assets/images/Instant1.png"
+// }
 const project4:Object = {
+    name : "Riot Fan API .NET remake",
+    link:"https://github.com/DemianFerreyra/API-Valorant-NET",
+    desc: "Proyecto de API de valorant, cambiando el lenguaje de Javscript con Express a C# con .NET Framework y Entity",
+    img: "assets/images/ValoAgents1.jpg"
+}
+const project5:Object = {
     name : "Riot Fan API",
     link:"https://github.com/DemianFerreyra/riotfanapi",
     desc: "Primer proyecto realizado en AWS, una API de personajes de RiotGames. Actualmente esta API solo dispone de personajes de Valorant, pero la idea es expandirla a personajes de League of Legends tambien. Cuenta con el back realizado en ExpressJS y el front en React + TypeScript",
     img: "assets/images/ValoAgents1.jpg"
 }
-const project5:Object = {
+const project6:Object = {
     name : "Loop Gaming",
     desc: "Proyecto cancelado. La idea era crear una red social estilo twitter mezclado con instagram, pero dedicada a la comunidad gamer. Este proyecto me sirvio principalmente para practicar y mejorar mi UI/UX",
     img: "assets/images/loopgaming.jpeg"
 }
-const project6:Object = {
+const project7:Object = {
     name : "Reto Mundial",
     link:"https://github.com/DemianFerreyra/retoMundialAPI",
     desc: "Proyecto grupal realizado durante una hackaton organizada por Biztrato. El proyecto tenia como tematica el mundial de futbol de qatar 2022, y la idea era que los usuarios pudiesen votar por cada partido, y que en base a esas votaciones, el front pudiera mostrar los equipos favoritos a ganar el mundial, los partidos y la cantidad de votos que recibio cada equipo, entre otros mas datos",
@@ -35,5 +41,5 @@ const project6:Object = {
 }
 
 export default[
-    project1, project2, project3, project4, project5, project6
+    project1, project2, project4, project5, project6, project7
 ]
